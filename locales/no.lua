ffc94739ad5380484de204e07ad1a9d6bbac38e9
welliux <youbetterplayz@gmail.com>
2020-11-10 11:26:08 +0100

    Creating a new locale.
    Made a support for norwegian.

---
+++ locales/no.lua
@@ -0,0 +1,6 @@
+Locales['no] = {
+  ['skin_menu'] = 'Hudmeny',
+  ['use_rotate_view'] = 'Bruk ~INPUT_VEH_FLY_ROLL_LEFT_ONLY~ og ~INPUT_VEH_FLY_ROLL_RIGHT_ONLY~ for å rotere.',
+  ['skin'] = 'Bytt hud',
+  ['saveskin'] = 'Lagre hud',
+}
