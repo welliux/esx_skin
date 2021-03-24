74cccfd5262220a2d7d4d83f35e097c01f6161a6
welliux <youbetterplayz@gmail.com>
2021-03-24 23:15:03 +0100

    Created no.lua

---
+++ locales/no.lua
@@ -0,0 +1,6 @@
+Locales['en'] = {
+  ['skin_menu'] = 'Skin Meny',
+  ['use_rotate_view'] = 'Bruk ~INPUT_VEH_FLY_ROLL_LEFT_ONLY~ og ~INPUT_VEH_FLY_ROLL_RIGHT_ONLY~ for å rotere kameraet.',
+  ['skin'] = 'Bytt antrekk',
+  ['saveskin'] = 'Lagre antrekk',
+}
